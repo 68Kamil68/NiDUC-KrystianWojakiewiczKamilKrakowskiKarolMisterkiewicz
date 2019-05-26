df6f148d84127a0b131cddc9d89f776952015f67
Karol Misterkiewicz <39039865+KarambAlky@users.noreply.github.com>
2019-05-26 18:06:45 +0200

    updated/working with bits V3.4 scrambler
    previous version wasn't working properly

--- V34.py
+++ V34.py
@@ -4,8 +4,7 @@ scrambler_output = []
 descrambler_output = []
 first_sync = []
 
-
-# Definicja sumy XOR
+#Definicja sumy XOR
 def xor(a, b):
     if int(a) - int(b) == 0:
         return 0
@@ -13,7 +12,7 @@ def xor(a, b):
         return 1
 
 
-# Tworzenie sync/poczatkowe 23 pseudolosowych bitow w scramblerze
+#Tworzenie sync/poczatkowe 23 pseudolosowych bitow w scramblerze
 def fill_sync(tab, tab2):
     for i in range(23):
         tab.append(random.randint(0, 1))
@@ -23,29 +22,30 @@ def fill_sync(tab, tab2):
 
 
 
-# wypełnienie scramblera i wypisanie poczatkowych liczb pseudolosowych
+#wypełnienie scramblera i wypisanie poczatkowych liczb pseudolosowych
 fill_sync(sync, first_sync)
 informal_sync = [str(i) for i in sync]
-print('\nCiag losowy SYNC: ' + ''.join(informal_sync))
+print('\nCiag losowy SYNC:    ' + ''.join(informal_sync))
 
-# Pobranie słowa z pliku
+#Pobranie słowa z pliku
 raw_binary = open('bintext.txt', 'r').read()
 print("Przed scramblingiem: " + raw_binary)
 
 
-# funkcja scramblujaca
+#funkcja scramblujaca
 def scrambling(tab):
     for i in range(len(tab)):
         temp = len(sync)
-        scrambler_output.append(xor(xor(sync[17], sync[22]), tab[i]))
+        tempo = xor(xor(sync[17], sync[22]), tab[i])
+        scrambler_output.append(tempo)
         while temp > 1:
             sync[temp-1] = sync[temp-2]
             temp -= 1
-        sync[0] = xor(xor(sync[17], sync[22]), tab[i])
+        sync[0] = tempo
     return scrambler_output
 
 
-# funkcja descramblujaca
+#funkcja descramblujaca
 def descrambling(tab):
     for i in range(len(tab)):
         temp = len(first_sync)
@@ -54,16 +54,17 @@ def descrambling(tab):
             first_sync[temp-1] = first_sync[temp-2]
             temp -= 1
         first_sync[0] = scrambler_output[i]
-    return scrambler_output
+    return descrambler_output
 
 
-# wykonanie scramblingu i wpisanie efektow
+#wykonanie scramblingu i wpisanie efektow
 scrambling(raw_binary)
 informal_scrambled = [str(i) for i in scrambler_output]
-print('Po scramblingu: ' + ''.join(informal_scrambled))
+print('Po scramblingu:      ' + ''.join(informal_scrambled))
 
 
-# wykonanie descramblingu i wpisanie efektow
+
+#wykonanie descramblingu i wpisanie efektow
 descrambling(scrambler_output)
 informal_descrambled = [str(i) for i in descrambler_output]
-print('Po descramblingu: ' + ''.join(informal_descrambled))
+print('Po descramblingu:    ' + ''.join(informal_descrambled))
